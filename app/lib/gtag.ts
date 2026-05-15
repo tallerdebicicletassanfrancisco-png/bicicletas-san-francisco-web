1763a1b1144ed9f5112cf4024b199aa7df2c18ea
José Alfonso Martínez Serrano <josemartinez@MacBook-Air-de-Jose.local>
2026-05-15 11:17:15 -0600

    Fix analytics events tracking

--- app/lib/gtag.ts
+++ app/lib/gtag.ts
@@ -1,20 +1,17 @@
 export const GA_MEASUREMENT_ID = "G-SHY8FHP9RT";
 
-declare global {
-  interface Window {
-    gtag: (...args: any[]) => void;
-  }
-}
-
 export const trackEvent = (
   action: string,
   category: string,
   label: string
 ) => {
-  if (typeof window !== "undefined" && window.gtag) {
-    window.gtag("event", action, {
+  if (typeof window !== "undefined") {
+    // @ts-ignore
+    window.gtag?.("event", action, {
       event_category: category,
       event_label: label,
     });
+
+    console.log("Evento enviado:", action);
   }
 };
